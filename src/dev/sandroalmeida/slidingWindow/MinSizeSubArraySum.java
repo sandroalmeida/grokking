28632a0824ea1ae2a5448925d72bc68aeb4096b7
sandroalmeida <sandro.almeida@dotbuild.com.br>
2020-08-20 16:04:19 -0700

    MinSizeSubArraySum implementation.

--- src/dev/sandroalmeida/slidingWindow/MinSizeSubArraySum.java
+++ src/dev/sandroalmeida/slidingWindow/MinSizeSubArraySum.java
@@ -2,6 +2,25 @@ package dev.sandroalmeida.slidingWindow;
 
 public class MinSizeSubArraySum {
 
+    public static int findMinsubArray(int S, int[] arr){
+        int start = 0, sum = 0, length, minLength = Integer.MAX_VALUE;
+        for(int end = 0; end < arr.length; end ++){
+            sum += arr[end];
+            while(sum >= S){
+                length = end - start + 1;
+                minLength = Math.min(minLength, length);
+                sum -= arr[start];
+                start++;
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 2, 3, 2};
+        System.out.println(findMinsubArray(7, arr));
+        int[] arr2 = {2, 1, 5, 2, 8};
+        System.out.println(findMinsubArray(7, arr2));
+    }
 
 }
