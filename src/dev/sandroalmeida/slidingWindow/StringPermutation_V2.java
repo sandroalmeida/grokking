79f5c586a2746eb047f8635f42130104b20a3f6d
sandroalmeida <sandro.almeida@dotbuild.com.br>
2020-08-28 09:18:49 -0700

    StringPermutation V2 implementation.

---
+++ src/dev/sandroalmeida/slidingWindow/StringPermutation_V2.java
@@ -0,0 +1,48 @@
+package dev.sandroalmeida.slidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringPermutation_V2 {
+
+    public static boolean findPermutation(String str, String pattern){
+
+        int start = 0, matched = 0;
+
+        Map<Character, Integer> charMap = new HashMap<>();
+        for(char c : pattern.toCharArray()){
+            charMap.put(c, charMap.getOrDefault(c, 0) + 1);
+        }
+
+        for(int end = 0; end < str.length(); end++){
+            char charRight = str.charAt(end);
+            if(charMap.containsKey(charRight)){
+                charMap.put(charRight, charMap.get(charRight) - 1);
+                if(charMap.get(charRight) == 0){
+                    matched++;
+                }
+            }
+
+            if(matched == charMap.size())
+                return true;
+
+            if(end > pattern.length()){
+                char leftChar = str.charAt(start);
+                if(charMap.containsKey(leftChar)){
+                    if(charMap.get(leftChar) == 0)
+                        matched--;
+                    charMap.put(leftChar, charMap.get(leftChar) + 1);
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation("oidbcaf", "abc"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation("odicf", "dc"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation("bcdxabcdy", "bcdyabcdx"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation("aaacb", "abc"));
+    }
+
+}
