cc23397a1f0235b866d31842fc81ce8b3af45194
sandroalmeida <sandro.almeida@dotbuild.com.br>
2020-09-27 05:27:07 -0700

    Permutations implemented.

---
+++ src/dev/sandroalmeida/subsets/Permutations.java
@@ -0,0 +1,38 @@
+package dev.sandroalmeida.subsets;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Permutations {
+
+    public static List<List<Integer>> findPermutations(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<List<Integer>> permutations = new LinkedList<>();
+
+        permutations.add(new ArrayList<>());
+
+        for(int currentNumber: nums){
+            int n = permutations.size();
+            for(int i = 0; i < n; i++){
+                List<Integer> oldPermutation = permutations.poll();
+                for(int j = 0; j <= oldPermutation.size(); j++){
+                    List<Integer> newPermutation = new ArrayList<>(oldPermutation);
+                    newPermutation.add(j, currentNumber);
+                    if(newPermutation.size() == nums.length)
+                        result.add(newPermutation);
+                    else
+                        permutations.add(newPermutation);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = Permutations.findPermutations(new int[] { 1, 3, 5 });
+        System.out.print("Here are all the permutations: " + result);
+    }
+}
