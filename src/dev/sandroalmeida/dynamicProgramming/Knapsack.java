274c14668be56ab1a91e5f54ac7349b2a84f731c
sandroalmeida <sandro.almeida@dotbuild.com.br>
2020-11-16 07:35:28 -0800

    Knapsack implemented.

---
+++ src/dev/sandroalmeida/dynamicProgramming/Knapsack.java
@@ -0,0 +1,37 @@
+package dev.sandroalmeida.dynamicProgramming;
+
+public class Knapsack {
+
+    public int solveKnapsack(int[] profits, int[] weights, int capacity) {
+        return this.knapsackRecursive(profits, weights, capacity, 0);
+    }
+
+    private int knapsackRecursive(int[] profits, int[] weights, int capacity, int currentIndex) {
+        if(capacity <= 0 || currentIndex >= profits.length)
+            return 0;
+
+        int profit1 = 0;
+        // if the weight of the element at currentIndex exceeds the capacity, we shouldn't process this
+        if(weights[currentIndex] <= capacity)
+            // recursive call after choosing the element at the currentIndex
+            profit1 = profits[currentIndex] + knapsackRecursive(profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+
+        // recursive call after excluding the element at the currentIndex
+        int profit2 = knapsackRecursive(profits, weights, capacity, currentIndex + 1);
+
+        return Math.max(profit1, profit2);
+
+    }
+
+
+    public static void main(String[] args) {
+        Knapsack ks = new Knapsack();
+        int[] profits = {1, 6, 10, 16};
+        int[] weights = {1, 2, 3, 5};
+        int maxProfit = ks.solveKnapsack(profits, weights, 7);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+        maxProfit = ks.solveKnapsack(profits, weights, 6);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+    }
+
+}
